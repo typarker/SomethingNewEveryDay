600b12a471489a0f9ac9e5096fef97236c00622e
ty <spinchill.ty@gmail.com>
2014-12-10 09:40:39 -0500

    You can now delete rows in the table, it needs to communicate to Parse and "delete" actual data now

--- SomethingNewEveryDay/WhatsBeenDoneTableViewController.swift
+++ SomethingNewEveryDay/WhatsBeenDoneTableViewController.swift
@@ -78,13 +78,9 @@ class WhatsBeenDoneTableViewController: UITableViewController {
 
     
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        //let cell:UITableViewCell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath) as UITableViewCell
-
-        // Configure the cell...
-        let cell:UITableViewCell = UITableViewCell(style:UITableViewCellStyle.Default, reuseIdentifier:"cell")
-        //cell.textLabel.text = tableData[indexPath.row]
         
-        //return cell
+        
+        var cell = tableView.dequeueReusableCellWithIdentifier("numberCell") as UITableViewCell
         
         let cellDataParse:PFObject = self.dataParse.objectAtIndex(indexPath.row) as PFObject
         
@@ -94,32 +90,30 @@ class WhatsBeenDoneTableViewController: UITableViewController {
     }
 
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
     
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            // Delete the row from the data source
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            dataParse.removeObjectAtIndex(indexPath.row)
+            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Automatic)
+        }
     }
     
 
-    /*
+    
     // Override to support rearranging the table view.
     override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
 
     }
-    */
+    
 
     /*
     // Override to support conditional rearranging of the table view.
